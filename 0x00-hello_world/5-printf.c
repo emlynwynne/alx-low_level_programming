cfac121fc886cc91a119ea063198f6b7dbccc01c
emlyn wynne <emlynwynne24@gmail.com>
2023-08-10 14:56:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main -A program print a line using using the printf function
+ * Return:0 (Sucess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return(0);
+}	
