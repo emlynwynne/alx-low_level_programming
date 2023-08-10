dc162fac97e97d8b443cc637d26c5aa3a14ddd05
emlyn wynne <emlynwynne24@gmail.com>
2023-08-10 14:43:47 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a lin that puts function.
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+le");
+return(0)
+}	
